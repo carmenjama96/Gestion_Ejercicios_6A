f56d3572e231cc5a140903282c750900352e85fb
Carmen <Carmen@192.168.163.1>
2016-07-14 23:54:19 -0500

    Archivos iniciales

--- Ejercicios_GestionCalidad/src/ejercicios_gestioncalidad/Ejercicios_Gestion_I.java
+++ Ejercicios_GestionCalidad/src/ejercicios_gestioncalidad/Ejercicios_Gestion_I.java
@@ -3,6 +3,7 @@ import java.util.Scanner;
 
 public class Ejercicios_Gestion_I {
     Scanner entrada = new Scanner (System.in);
+    String valor_entrada;
     
     public void entrada_opcion(){
     System.out.println ("--------------------------------------------------------\n"
@@ -11,29 +12,31 @@ public class Ejercicios_Gestion_I {
                           + "--------------------------------------------------------\n"
                 + "1. Calcular el area y perimetro de un triangulo rectangulo\n");
         System.out.print("Opcion: ");
-        String opcion =entrada.next();
-        evaluar_opcion(opcion);
+        valor_entrada =entrada.next();
+        evaluar_opcion(valor_entrada);
     }
     
-    public void evaluar_opcion(String valor){
-        switch (valor) {
+    public void evaluar_opcion(String opcion){
+        switch (opcion) {
             case "1":
                 A_AreaPerimetroTriganguloRectangulo ejercicio_1= new A_AreaPerimetroTriganguloRectangulo();
                 ejercicio_1.entrada_datos();
                 break;
             default:
                 System.out.print("Opcion no valida, ¿Desea intentarlo de nuevo? (SI/NO): ");
-                String opcion =entrada.next();
-                if(opcion.equalsIgnoreCase("SI")){
+                valor_entrada =entrada.next();
+                if(valor_entrada.equals("SI")||valor_entrada.equals("si")||valor_entrada.equals("Si")){
                     System.out.print("\n\n\n\n\n\n");
                     entrada_opcion();
                 }else{
-                    break;
+                    System.exit(0);
                 }
         }
     }
     public static void main(String[] args) {
         Ejercicios_Gestion_I ejecucion = new Ejercicios_Gestion_I();
         ejecucion.entrada_opcion();
+        A_AreaPerimetroTriganguloRectangulo ejercicio_1= new A_AreaPerimetroTriganguloRectangulo();
+        ejercicio_1.entrada_datos();
     }
 }
